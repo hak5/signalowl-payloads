1e5bafda4d77c7deebee92008e6e1170d72d8f57
Marc <foxtrot@realloc.me>
2019-08-07 17:12:10 +0000

    Add Initial Payloads (#1)
    Add Initial Payloads.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Hak5 Owl Payloads Repository
+# Payload Library for the Signal Owl by Hak5
+
+![Signal Owl](https://cdn.shopify.com/s/files/1/0068/2142/files/signal-owl_400x.jpg "Signal Owl")
+
+* [Purchase](https://shop.hak5.org/products/signal-owl "Purchase at Hak5.org")
+* [Forums](https://forums.hak5.org/forum/98-signal-owl/ "Signal Owl Forums")
+* [Documentation](https://docs.hak5.org/hc/en-us/categories/360002117953-Signal-Owl "Signal Owl Documentation")
+* [Downloads](https://downloads.hak5.org/ "Signal Owl Downloads")
---
+++ payloads/extensions/wifi_connect.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Title: WIFI_CONNECT
+# Description: Simplifies WiFi client mode connection. Expects $WIFI_SSID and $WIFI_PASS
+# Author: Hak5Darren
+
+function WIFI_CONNECT() {
+    logger running extension: wifi_connect
+    ifconfig wlan0 up;sleep 2
+    echo -e "network={\nssid=\"$WIFI_SSID\"\npsk=\"$WIFI_PASS\"\npriority=1\n}">/tmp/wpa.conf
+    wpa_supplicant -B -Dnl80211 -i wlan0 -c /tmp/wpa.conf
+    while(iwconfig wlan0 | grep Not-Associated); do sleep 1; done
+    udhcpc -i wlan0
+}
+export -f WIFI_CONNECT
